c5f1cae4a311449f3bb54dee39edbeb6639cb4c3
Jason <50128317+japanesecoffee@users.noreply.github.com>
2022-11-12 23:45:56 -0500

    Upload camera pictures to Firebase Cloud Storage

--- OutfitGenerator/Controllers/ViewController.swift
+++ OutfitGenerator/Controllers/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Jason on 10/14/22.
 //
 
+import FirebaseStorage
 import UIKit
 
 class ViewController: UIViewController {
@@ -28,6 +29,25 @@ class ViewController: UIViewController {
         imagePicker.delegate = self
         present(imagePicker, animated: true)
     }
+    
+    // Uploads images to Firebase Cloud Storage.
+    func uploadImage(_ image: UIImage) {
+        let storageReference = Storage.storage().reference()
+        
+        guard let imageData = image.pngData() else {
+            return
+        }
+        
+        // Each uploaded image gets a unique ID.
+        let imagesReference = storageReference.child("images/\(UUID().uuidString).png")
+        
+        imagesReference.putData(imageData, metadata: nil) { (_, error) in
+            guard error == nil else {
+                print("Failed to upload")
+                return
+            }
+        }
+    }
 }
 
 // MARK: - UIImagePickerControllerDelegate and UINavigationControllerDelegate methods
@@ -47,5 +67,7 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
         guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
             return
         }
+        
+        uploadImage(image)
     }
 }
