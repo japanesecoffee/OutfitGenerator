eee8774dd7d49b4da466bd2d67807208db1d54c7
Jason <50128317+japanesecoffee@users.noreply.github.com>
2023-02-09 22:24:03 -0500

    Create a custom collection view cell

---
+++ OutfitGenerator/Views/ClothingCollectionViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  ClothingCollectionViewCell.swift
+//  OutfitGenerator
+//
+//  Created by Jason on 2/9/23.
+//
+
+import UIKit
+
+class ClothingCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "ClothingCollectionViewCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}
