3f5b1bb094f5a6fa7f1f85e69c688794d7bcbcaf
Jason <50128317+japanesecoffee@users.noreply.github.com>
2023-07-13 00:26:00 -0400

    Show an outfit on the generator screen

--- OutfitGenerator/Controllers/GeneratorViewController.swift
+++ OutfitGenerator/Controllers/GeneratorViewController.swift
@@ -5,25 +5,78 @@
 //  Created by Jason on 6/29/23.
 //
 
+import FirebaseDatabase
+import FirebaseStorage
 import UIKit
 
 class GeneratorViewController: UIViewController {
+    
+    private var generatorView = GeneratorView()
+    
+    private var topsDatabaseReference: DatabaseReference!
+    private var bottomsDatabaseReference: DatabaseReference!
+    private var shoesDatabaseReference: DatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .white
+        view = generatorView
+        
+        topsDatabaseReference = Database.database().reference().child("tops")
+        bottomsDatabaseReference = Database.database().reference().child("bottoms")
+        shoesDatabaseReference = Database.database().reference().child("shoes")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        loadOutfit()
     }
-    */
-
+    
+    private func loadOutfit() {
+        let storageReference = Storage.storage().reference()
+        
+        let placeholderImage = UIImage(
+            systemName: "photo"
+        )?.withTintColor(
+            .systemGray6,
+            renderingMode: .alwaysOriginal
+        )
+        
+        topsDatabaseReference.observeSingleEvent(of: .childAdded) { (snapshot) in
+            if snapshot.exists() {
+                guard let value = snapshot.value as? String else {
+                    return
+                }
+                self.generatorView.topsImageView.sd_setImage(
+                    with: storageReference.child(value),
+                    placeholderImage: placeholderImage
+                )
+            }
+        }
+        
+        bottomsDatabaseReference.observeSingleEvent(of: .childAdded) { (snapshot) in
+            if snapshot.exists() {
+                guard let value = snapshot.value as? String else {
+                    return
+                }
+                self.generatorView.bottomsImageView.sd_setImage(
+                    with: storageReference.child(value),
+                    placeholderImage: placeholderImage
+                )
+            }
+        }
+        
+        shoesDatabaseReference.observeSingleEvent(of: .childAdded) { (snapshot) in
+            if snapshot.exists() {
+                guard let value = snapshot.value as? String else {
+                    return
+                }
+                self.generatorView.shoesImageView.sd_setImage(
+                    with: storageReference.child(value),
+                    placeholderImage: placeholderImage
+                )
+            }
+        }
+    }
 }
---
+++ OutfitGenerator/Views/GeneratorView.swift
@@ -0,0 +1,67 @@
+//
+//  GeneratorView.swift
+//  OutfitGenerator
+//
+//  Created by Jason on 7/12/23.
+//
+
+import UIKit
+
+class GeneratorView: UIView {
+
+    var topsImageView = UIImageView()
+    var bottomsImageView = UIImageView()
+    var shoesImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .white
+        
+        let stackView = UIStackView()
+        stackView.clipsToBounds = true
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+        stackView.isLayoutMarginsRelativeArrangement = true
+        addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+        
+        stackView.addArrangedSubview(topsImageView)
+        topsImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.addArrangedSubview(bottomsImageView)
+        bottomsImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.addArrangedSubview(shoesImageView)
+        shoesImageView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let statusBarHeight = self.window?.windowScene?.statusBarManager?.statusBarFrame.height
+        let tabBarController = self.window?.rootViewController as! UITabBarController
+        let tabBarHeight = tabBarController.tabBar.frame.size.height
+        let height = self.frame.size.height - statusBarHeight! - tabBarHeight
+        
+        topsImageView.heightAnchor.constraint(equalToConstant: height / 3).isActive = true
+        topsImageView.widthAnchor.constraint(equalToConstant: height / 3).isActive = true
+        
+        bottomsImageView.heightAnchor.constraint(equalToConstant: height / 3).isActive = true
+        bottomsImageView.widthAnchor.constraint(equalToConstant: height / 3).isActive = true
+        
+        shoesImageView.heightAnchor.constraint(equalToConstant: height / 3).isActive = true
+        shoesImageView.widthAnchor.constraint(equalToConstant: height / 3).isActive = true
+    }
+}
