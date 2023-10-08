512c0eecd91ff22a22b7258cbc1363736eb3a904
Jason <50128317+japanesecoffee@users.noreply.github.com>
2023-10-08 15:13:28 -0400

    Deselect row after tapping

--- OutfitGenerator/Controllers/SettingsViewController.swift
+++ OutfitGenerator/Controllers/SettingsViewController.swift
@@ -70,4 +70,8 @@ extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
         cell.textLabel?.text = option
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
